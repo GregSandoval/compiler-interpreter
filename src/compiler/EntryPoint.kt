3733357589f7d15b36cda2437277c8150576a0a7
Gregory A. Sandoval <gregorysandoval.a@gmail.com>
2020-05-25 13:28:28 -0700

    Extract AST Validator to separate class.

--- src/compiler/EntryPoint.kt
+++ src/compiler/EntryPoint.kt
@@ -3,13 +3,10 @@ package compiler
 import compiler.a5.lexer.A5Lexer
 import compiler.a5.parser.A7Parser
 import compiler.interpreter.Interpreter
+import compiler.parser.ASTValidator
 import compiler.parser.AbstractSyntaxTreeBuilder
-import compiler.parser.Symbol.NonTerminal
-import compiler.parser.Symbol.NonTerminal.ParseTreeSentinel
 import compiler.parser.Symbol.Terminal
-import compiler.parser.TreeNode
 import visualization.TreeVisualizer
-import java.util.*
 
 object EntryPoint {
     var inputName: String? = null
@@ -38,38 +35,12 @@ object EntryPoint {
         // Transform PST to AST (in-place)
         AbstractSyntaxTreeBuilder.fromParseTree(root)
 
+        ASTValidator.validate(root)
+
         // Serialize ASt
         TreeVisualizer.toImage(root, inputs.astFileName)
         root = root.children[0]
 
         Interpreter.execute(root)
     }
-
-    fun validateAST(tree: TreeNode) {
-        println("Validating AST contains only tokens...")
-        val unhandledNodes = ArrayList<NonTerminal>()
-        validateAST(tree, unhandledNodes)
-
-        if (unhandledNodes.isEmpty()) {
-            println("AST contains only tokens!")
-            return
-        }
-
-        if (unhandledNodes.size == 1 && unhandledNodes[0] !is ParseTreeSentinel)
-            println("Uh-oh; AST contains grammar nodes! Need to add more logic to these nodes:$unhandledNodes")
-    }
-
-    fun validateAST(tree: TreeNode?, unhandledNodes: MutableList<NonTerminal>) {
-        if (tree == null) {
-            return
-        }
-        if (tree is NonTerminal) {
-            unhandledNodes.add(tree)
-        }
-        for (child in tree.children) {
-            validateAST(child, unhandledNodes)
-        }
-    }
-
-
 }
