b827b1f36deb7dce3aaae7fe7801100523fcab33
Gregory A. Sandoval <gregorysandoval.a@gmail.com>
2020-05-25 15:38:18 -0700

    Introduce pst visitor.

--- src/compiler/parser/AbstractSyntaxTreeBuilder.kt
+++ src/compiler/parser/AbstractSyntaxTreeBuilder.kt
@@ -2,44 +2,42 @@ package compiler.parser
 
 import compiler.a5.grammar.PstToAstNonTerminalVisitor
 import compiler.a5.grammar.PstToAstTokenVisitor
-import compiler.parser.Symbol.NonTerminal
-import compiler.parser.Symbol.Terminal
+
 
 object AbstractSyntaxTreeBuilder {
+
     fun fromParseTree(tree: TreeNode) {
-        postordervisit(tree, PstToAstTokenVisitor(), PstToAstNonTerminalVisitor())
+        ParseTreeVisitor.postordervisit(tree, createVisitor())
     }
 
-    fun postordervisit(tree: TreeNode?, tokenVisitor: PstToAstTokenVisitor, nonTerminalVisitor: PstToAstNonTerminalVisitor) {
-        if (tree == null) {
-            return
+    fun createVisitor(): (TreeNode) -> Unit {
+        val tokenVisitor = PstToAstTokenVisitor()
+        val nonTerminalVisitor = PstToAstNonTerminalVisitor()
+
+        return fun(tree: TreeNode) {
+            // Remove NonTerminals that derive epsilon
+            trim(tree)
+
+            // Contract NonTerminal chains
+            contract(tree)
+
+            if (tree is Symbol.NonTerminal) {
+                nonTerminalVisitor.accept(tree)
+            }
+
+            if (tree is Symbol.Terminal) {
+                tokenVisitor.accept(tree)
+            }
         }
 
-        for (child in tree.children) {
-            postordervisit(child, tokenVisitor, nonTerminalVisitor)
-        }
-
-        // Remove NonTerminals that derive epsilon
-        trim(tree)
-
-        // Contract NonTerminal chains
-        contract(tree)
-
-        if (tree is NonTerminal) {
-            nonTerminalVisitor.accept(tree)
-        }
-
-        if (tree is Terminal) {
-            tokenVisitor.accept(tree)
-        }
     }
 
     private fun trim(tree: TreeNode) {
-        tree.children.removeIf { it is NonTerminal && it.children.isEmpty() }
+        tree.children.removeIf { it is Symbol.NonTerminal && it.children.isEmpty() }
     }
 
     private fun contract(tree: TreeNode) {
-        if (tree is NonTerminal && tree.children.size == 1) {
+        if (tree is Symbol.NonTerminal && tree.children.size == 1) {
             val child = tree.children[0]
             child.parent = tree.parent
             tree.parent!!.children[tree.parent!!.children.indexOf(tree)] = child
@@ -47,4 +45,5 @@ object AbstractSyntaxTreeBuilder {
             tree.children.clear()
         }
     }
+
 }
