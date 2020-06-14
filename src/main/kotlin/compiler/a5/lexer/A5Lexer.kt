88781301bef9f0d2ea208a238d7f570114604dee
Gregory A. Sandoval <gregorysandoval.a@gmail.com>
2020-06-14 13:29:25 -0700

    Add back lexer logging.
    :)

--- src/main/kotlin/compiler/a5/lexer/A5Lexer.kt
+++ src/main/kotlin/compiler/a5/lexer/A5Lexer.kt
@@ -1,6 +1,7 @@
 package compiler.a5.lexer
 
 import compiler.a5.lexicon.A5LexiconDFA
+import compiler.dfa.DFALogger
 import compiler.dfa.LexicalNode
 import compiler.lexer.LexerBuilder
 import compiler.parser.Symbol.Terminal
@@ -10,6 +11,8 @@ object A5Lexer {
     fun lex(text: String): List<Terminal> {
         return LexerBuilder()
                 .setDFA(A5LexiconDFA())
+                .onTransition(DFALogger::logTransition)
+                .onTokenCreated(DFALogger::logAcceptedToken)
                 .onUnknownTokenFound(this::logUnknownToken)
                 .createLexer()
                 .lex(text)
